1452b94166eeaca409d3c10e4f895e5dfb0a1ab3
Xerinzxx <Srinivasy369@gmail.com>
2024-08-09 22:24:44 +0530

    Add build folder

---
+++ build/precache-manifest.2f12914c882de6dedf7a63591387c7bc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfb148b28d2910a905c4e516554e23a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec522e708a7d0046cb9c",
+    "url": "/static/css/main.22337fdc.chunk.css"
+  },
+  {
+    "revision": "fd881a44bef3f2d97cad",
+    "url": "/static/js/2.b22793ad.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.b22793ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec522e708a7d0046cb9c",
+    "url": "/static/js/main.08b49482.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  },
+  {
+    "revision": "3195ba389f03b9db7da796d65487f642",
+    "url": "/static/media/azure.3195ba38.svg"
+  },
+  {
+    "revision": "43bab1d315c7180dd80a2599ebff64ef",
+    "url": "/static/media/css.43bab1d3.svg"
+  },
+  {
+    "revision": "4fdb93e864fbf98bef270db23ff1a21e",
+    "url": "/static/media/docker.4fdb93e8.svg"
+  },
+  {
+    "revision": "bfa69c4f3255d5c564c2ec600ff9885e",
+    "url": "/static/media/firebase.bfa69c4f.svg"
+  },
+  {
+    "revision": "8aec90151eb808273c6c8a0f5785c880",
+    "url": "/static/media/git.8aec9015.svg"
+  },
+  {
+    "revision": "03a8c63d00f0dd2f40fb0ad11d5db862",
+    "url": "/static/media/html.03a8c63d.svg"
+  },
+  {
+    "revision": "aefb563ed2048c9d797ac592ad89e764",
+    "url": "/static/media/jenkins.aefb563e.svg"
+  },
+  {
+    "revision": "b373ef75fc471bce3cef3175a1420669",
+    "url": "/static/media/kubernetes.b373ef75.svg"
+  },
+  {
+    "revision": "2aa7f7b569fd6e9176f49dd31eaa970e",
+    "url": "/static/media/mongodb.2aa7f7b5.svg"
+  },
+  {
+    "revision": "94cafb0d1b4a2875215db17964f0938a",
+    "url": "/static/media/nodejs.94cafb0d.svg"
+  }
+]);
